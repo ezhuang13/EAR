97716ecce860b59b17e2f66ddb1b767a31280d72
ezhuang13 <ezhuang13@gmail.com>
2019-04-18 15:51:21 -0500

    changing settings for deployment

--- source/server/app.py
+++ source/server/app.py
@@ -31,4 +31,4 @@ def create_app():
 # Create and Run the Application here.
 if __name__ == '__main__':
 	app = create_app()
-	app.run(debug=True, host='0.0.0.0')
+	app.run(debug=False, host='0.0.0.0')
--- source/server/config.py
+++ source/server/config.py
@@ -3,9 +3,9 @@ import os
 
 # Development Configuration for App.py
 class Development(object):
-	DEBUG = True
+	DEBUG = False
 	TESTING = False
-	FLASK_ENV = 'development'
+	FLASK_ENV = 'production'
 
 	# Need to export the database_url variable (as detailed in the README)
 	SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
